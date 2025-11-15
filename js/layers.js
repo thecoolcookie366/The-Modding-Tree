11b8266e0f51b6470e0dc3580b5afa9f789f9f7b
thecoolcookie366 <dariusalexandrubalahura@gmail.com>
2025-11-15 12:51:27 +0200

    red layer
    hopefully this red layer works

--- js/layers.js
+++ js/layers.js
@@ -35,8 +35,8 @@ addLayer("cc", {
         },
 
         13: {
-            title: "Some Other Boost",
-            description: "Testing 123",
+            title: "Chocolate!",
+            description: "Boost cookies based on chocolate cookies.",
             cost: new Decimal(1e303),
                 effect() {
         return player[this.layer].points.add(1).pow(0.5)
@@ -47,6 +47,44 @@ addLayer("cc", {
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "c", description: "C: Reset for chocolate cookies", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+
+    
+})
+addLayer("r", {
+    name: "red", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#ff0000ff",
+    requires: new Decimal(1e33), // Can be a function that takes requirement increases into account
+    resource: "red", // Name of prestige currency
+    baseResource: "cookies", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    upgrades: {
+        11: {
+            title: "Cookie Duplication",
+            description: "Your first upgrade. Get x2 cookies.",
+            cost: new Decimal(1),
+        },
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "r", description: "R: Reset for red", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 
