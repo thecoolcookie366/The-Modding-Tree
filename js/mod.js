40e5ed4b32836a95f4b2bb3a8c7d7b02ad01058b
thecoolcookie366 <dariusalexandrubalahura@gmail.com>
2025-11-15 10:59:28 +0200

    mod.js update
    the tree now has a name

--- js/mod.js
+++ js/mod.js
@@ -1,27 +1,27 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Cookie Tree",
+	author: "thecoolcookie366",
+	pointsName: "cookies",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+	initialStartPoints: new Decimal (1), // Used for hard resets and new players
+	offlineLimit: 24,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.01a",
+	name: "The Cookie Tree Release",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.01a</h3><br>
+		- First release. <br>
+		- Added cookies.`
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = `Congrats! The universe is now filled with cookies.`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
@@ -55,7 +55,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.points.gte(new Decimal("e33"))
 }
 
 
