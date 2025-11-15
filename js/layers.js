f9dddf5cadc47b7cd67b057ae60494e6c0e81319
thecoolcookie366 <dariusalexandrubalahura@gmail.com>
2025-11-15 14:58:39 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -153,7 +153,7 @@ addLayer("r", {
 
 addLayer("l", {
     name: "loops", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "🗘", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "L", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
