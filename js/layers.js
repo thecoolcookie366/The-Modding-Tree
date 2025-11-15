494804f70cfe92f59149e02c315e19edc107793e
thecoolcookie366 <dariusalexandrubalahura@gmail.com>
2025-11-15 13:41:31 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -24,19 +24,19 @@ addLayer("cc", {
     },
     upgrades: {
         11: {
-            title: "[#1] Cookie Duplication",
+            title: "[#1cc] Cookie Duplication",
             description: "Your first upgrade. Get x2 cookies.",
             cost: new Decimal(1),
         },
 
         12: {
-            title: "[#2] Strong Cookie Duplication",
+            title: "[#2cc] Strong Cookie Duplication",
             description: "Cookie gain go big. x3 cookies!",
             cost: new Decimal(5),
         },
 
         13: {
-            title: "[#3] Chocolate!",
+            title: "[#3cc] Chocolate!",
             description: "Boost cookies based on chocolate cookies.",
             cost: new Decimal(15),
                 effect() {
@@ -46,7 +46,7 @@ addLayer("cc", {
         },
 
         14: {
-            title: "[#4] Back and Forth",
+            title: "[#4cc] Back and Forth",
             description: "Boost chocolate cookies based on cookies.",
             cost: new Decimal(1e3),
                 effect() {
@@ -63,6 +63,46 @@ addLayer("cc", {
 
     
 })
+
+addLayer("dcc", {
+    name: "darkchocolatecookies", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "DCC", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#745c0dff",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "dark chocolate cookies", // Name of prestige currency
+    baseResource: "chocolate cookies", // Name of resource prestige is based on
+    baseAmount() {return player.cc.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    upgrades: {
+        11: {
+            title: "[#1dcc] Darker Than the Last",
+            description: "does nothing yet",
+            cost: new Decimal(1e1.79e308),
+        },
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "c", description: "C: Reset for chocolate cookies", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+
+    
+})
+
 addLayer("r", {
     name: "red", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
@@ -88,7 +128,7 @@ addLayer("r", {
     },
     upgrades: {
         11: {
-            title: "WHY IS IT RED",
+            title: "[#1r] WHY IS IT RED",
             description: "Uh... okay? x5 cookies.",
             cost: new Decimal(1),
         },
