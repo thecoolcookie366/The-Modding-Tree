43ac0485044ce12fdcc6d66a5c17d50a7febe146
thecoolcookie366 <dariusalexandrubalahura@gmail.com>
2025-11-16 11:46:17 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -523,7 +523,7 @@ addLayer("ml", {
     },
     row: 4, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "aaaaaaaaaaaaaaaa", description: "L: Attempt to loop.", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "l", description: "L: Attempt to loop.", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 
