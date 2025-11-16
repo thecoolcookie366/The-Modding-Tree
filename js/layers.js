b04b4a5046d2daebbdfb8949f8fdb85e6437dc55
thecoolcookie366 <dariusalexandrubalahura@gmail.com>
2025-11-16 15:03:32 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -541,7 +541,7 @@ addLayer("p", {
     baseResource: "cookies", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 2, // Prestige currency exponent
+    exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
