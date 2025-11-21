fd575cc2612d149f97470a776a41e5fbc853b256
thecoolcookie366 <dariusalexandrubalahura@gmail.com>
2025-11-21 21:26:31 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -322,7 +322,7 @@ addLayer("y", {
 		points: new Decimal(0),
     }},
     color: "#eeff00ff",
-    requires: new Decimal(5e9), // Can be a function that takes requirement increases into account
+    requires: new Decimal(3.5e9), // Can be a function that takes requirement increases into account
     resource: "yellow", // Name of prestige currency
     baseResource: "cookies", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
