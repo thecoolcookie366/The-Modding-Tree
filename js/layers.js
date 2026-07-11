b855953dd6e540cfd2c0c2b67148348d9dd37ad0
thecoolcookie366 <dariusalexandrubalahura@gmail.com>
2026-07-11 14:27:37 +0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1458,7 +1458,7 @@ addLayer("pie", {
     symbol: "π", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
+        unlocked: false,
 		points: new Decimal(0),
     }},
     color: "#d1634d",
